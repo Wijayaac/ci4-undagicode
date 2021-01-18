38f063594c00ee02bee37f1e32bed10eca1f29cd
Wijayaac <wijayaac25@gmail.com>
2021-01-18 16:51:57 +0800

    first commit

---
+++ app/Views/Hello.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World</title>
+    <link rel="stylesheet" href="<?= base_url() ?>/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?= base_url() ?>/assets/icon/css/all.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4">
+                <div class="bg-dark">
+                    <h4>Hello World</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="<?= base_url() ?>/assets/js/jquery-3.5.1.min.js"></script>
+    <script src="<?= base_url() ?>/assets/js/popper.min.js"></script>
+    <script src="<?= base_url() ?>/assets/js/bootstrap.min.js"></script>
+</body>
+
+</html>
